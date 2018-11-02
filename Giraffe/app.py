3137824f20e8707e82cc8954230757faa43368af
scopelius <fabian.knobloch@gmail.com>
2018-11-02 18:52:29 +0100

    Update Giraffe/app.py

---
+++ Giraffe/app.py
@@ -0,0 +1,22 @@
+from math import *
+
+print("Hello janni")
+
+character_name = "abc"
+
+print("  /")
+print(" /")
+print(" \\")
+print("  \\")
+
+print(character_name[1].upper().isupper())
+
+def hiii(name):
+    print("huhu " + name)
+
+hiii("ssss")
+
+
+
+
+
